e91e4a405a32ddcd7abc99f3c01dd71160d54a12
german.bf <german.bf@avangenio.com>
2024-07-08 16:43:18 -0400

    preparando el entorno

--- task/src/App.jsx
+++ task/src/App.jsx
@@ -1,11 +1,10 @@
 import React from "react"
+import Login from "./componet/login"
 
 export default function App() {
   return (
     <div className="text-center">
-      <h1 className="text-3xl font-bold underline">
-        Hello world!
-      </h1>
+      <Login/>
     </div>
   )
 }
