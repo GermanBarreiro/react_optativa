9d527f176abf18f677be024d8565dfa28ccd2f8d
german.bf <german.bf@avangenio.com>
2024-07-08 16:20:29 -0400

    initial comit

---
+++ task/src/App.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+export default function App() {
+  return (
+    <div className="text-center">
+      <h1 className="text-3xl font-bold underline">
+        Hello world!
+      </h1>
+    </div>
+  )
+}
